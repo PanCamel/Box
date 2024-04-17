cabcf92d18b7d6dda9cabc4b4a5eb8548139b0da
PanCamel <wbalucki18@gmail.com>
2024-04-17 18:28:21 +0200

    Added ToString

--- Pudelko/Class1.cs
+++ Pudelko/Class1.cs
@@ -1,4 +1,6 @@
-﻿namespace Pudelko;
+﻿using System.Globalization;
+
+namespace Pudelko;
 
 public enum UnitOfMeasure
 {
@@ -6,8 +8,8 @@ public enum UnitOfMeasure
     Centimeter,
     Meter
 }
-public class Pudelko
 
+public class Pudelko: IFormattable
 {
     private double[] dimensions = new double[3] { 0.1, 0.1, 0.1 };
 
@@ -16,8 +18,8 @@ public class Pudelko
     private double B => dimensions[1];
     private double C => dimensions[2];
     
-    public double Volume => Math.Round(A * B * C, 9);
-    public double Area => Math.Round(2 * (A * B + B * C + A * C), 6);
+    public double Objetosc => Math.Round(A * B * C, 9);
+    public double Pole => Math.Round(2 * (A * B + B * C + A * C), 6);
     
     public Pudelko(double a = 0.1, double b = 0.1, double c = 0.1, UnitOfMeasure unit = UnitOfMeasure.Meter)
     {
@@ -48,5 +50,29 @@ public class Pudelko
             c /= 1000;
         }
         dimensions = new double[] { a, b, c };
+    }
+    public override string ToString()
+    {
+        return ToString("m", CultureInfo.InvariantCulture);
+    }
+
+    
+
+    public string ToString(string format, IFormatProvider formatProvider)
+    {
+        switch (format)
+        {
+            case "m":
+                return
+                    $"{A.ToString("0.000", formatProvider)} m × {B.ToString("0.000", formatProvider)} m × {C.ToString("0.000", formatProvider)} m";
+            case "cm":
+                return
+                    $"{(A * 100).ToString("0.0", formatProvider)} cm × {(B * 100).ToString("0.0", formatProvider)} cm × {(C * 100).ToString("0.0", formatProvider)} cm";
+            case "mm":
+                return
+                    $"{(A * 1000).ToString("0", formatProvider)} mm × {(B * 1000).ToString("0", formatProvider)} mm × {(C * 1000).ToString("0", formatProvider)} mm";
+            default:
+                throw new FormatException("Invalid format.");
+        }
     }
 }
